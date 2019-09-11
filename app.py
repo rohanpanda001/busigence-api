d5e435a6aa3a717c1405ccb66a2a8aeda2a8a7a7
Rohan Panda <rohan.panda1@gmail.com>
2019-09-12 04:14:10 +0530

    Fix join and sort

--- app.py
+++ app.py
@@ -14,12 +14,20 @@ app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 mysql.init_app(app)
 
 conn = mysql.connect()
-mycursor =conn.cursor()
+mycursor = conn.cursor()
+
+
+@app.after_request
+def add_header(response):
+    response.headers['Content-Type'] = 'application/json'
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    return response
 
 
 @app.route('/', methods=['GET'])
 def home():
     return 'hello'
+
 
 @app.route('/showDatabases', methods=['POST'])
 def showDatabases():
@@ -27,10 +35,12 @@ def showDatabases():
     result = mycursor.fetchall()
     return json.dumps(result)
 
+
 @app.route('/showTables', methods=['POST'])
 def showTables():
-    db = request.form['db']
-    mycursor.execute("use "+ db)
+    content = request.get_json()
+    db = content['db']
+    mycursor.execute("use " + db)
     mycursor.execute("show tables")
     result = mycursor.fetchall()
     return json.dumps(result)
@@ -38,7 +48,8 @@ def showTables():
 
 @app.route('/showRows', methods=['POST'])
 def showAll():
-    table = request.form['table']
+    content = request.get_json()
+    table = content['table']
     mycursor.execute("Select * from " + table)
     result = mycursor.fetchall()
     return json.dumps(result)
@@ -47,19 +58,36 @@ def showAll():
 @app.route('/proccessJoin', methods=['POST'])
 def proccessJoin():
     content = request.get_json()
-    join_type = content['join_type']
+    joinType = content['joinType']
+    primaryKey = content['primaryKey']
     tableMap = content['tables']
 
     seperator = ', '
     customers = tableMap['customers']
     orders = tableMap['orders']
 
-    df_a = pd.read_sql("Select " + seperator.join(customers) + " from customers", conn)
-    df_b = pd.read_sql("Select " + seperator.join(orders) + " from orders",conn)
-    
-    df_c = pd.merge(df_a, df_b,on='CustomerID',how=join_type)
-    response = df_c.to_dict()
-    return jsonify(response)
+    df_a = pd.read_sql(
+        "Select " + seperator.join(customers) + " from customers", conn)
+    df_b = pd.read_sql("Select " + seperator.join(orders) +
+                       " from orders", conn)
+
+    df_c = pd.merge(df_a, df_b, on=primaryKey, how=joinType)
+    response = df_c.to_json(orient='index')
+    return response
+
+
+@app.route('/proccessSort', methods=['POST'])
+def proccessSort():
+    content = request.get_json()
+    sortKey = content['sortKey']
+    isAscending = content['isAscending']
+    tableData = content['tableData']
+
+    df = pd.DataFrame.from_dict(tableData)
+
+    final_df = df.sort_values(by=sortKey, ascending=isAscending)
+    response = final_df.to_json(orient='index')
+    return response
 
 
 if __name__ == '__main__':
